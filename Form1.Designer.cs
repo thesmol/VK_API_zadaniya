e273aae600fb0804c2061f08e8de8a0d13deb921
IlQQQlI <ssm26032003@gmail.com>
2021-07-06 12:44:51 +0300

    Написать код к кнопке получения записи со стены.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,17 +35,18 @@ namespace VK_API_zadaniya
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.wallPost = new System.Windows.Forms.TextBox();
+            this.getwallpost = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 395);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(310, 335);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 54);
+            this.button1.Size = new System.Drawing.Size(164, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Показать список друзей";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,19 +55,17 @@ namespace VK_API_zadaniya
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(540, 44);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(512, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(310, 444);
+            this.listBox1.Size = new System.Drawing.Size(276, 228);
             this.listBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(26, 44);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Location = new System.Drawing.Point(23, 35);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(154, 54);
+            this.button2.Size = new System.Drawing.Size(137, 43);
             this.button2.TabIndex = 3;
             this.button2.Text = " Токен пользователя";
             this.button2.UseVisualStyleBackColor = true;
@@ -74,10 +73,9 @@ namespace VK_API_zadaniya
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(26, 116);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Location = new System.Drawing.Point(23, 93);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(154, 54);
+            this.button3.Size = new System.Drawing.Size(137, 43);
             this.button3.TabIndex = 4;
             this.button3.Text = "Токен сообщества";
             this.button3.UseVisualStyleBackColor = true;
@@ -85,58 +83,80 @@ namespace VK_API_zadaniya
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 179);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(23, 143);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(154, 144);
+            this.textBox1.Size = new System.Drawing.Size(137, 116);
             this.textBox1.TabIndex = 5;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(186, 44);
+            this.groupBox1.Location = new System.Drawing.Point(165, 35);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(249, 187);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(221, 150);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "показать";
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(33, 30);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(87, 24);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "друзья";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(33, 60);
+            this.radioButton2.Location = new System.Drawing.Point(29, 48);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(124, 24);
+            this.radioButton2.Size = new System.Drawing.Size(104, 21);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "подписчики";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(29, 24);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "друзья";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // wallPost
+            // 
+            this.wallPost.Location = new System.Drawing.Point(512, 256);
+            this.wallPost.Multiline = true;
+            this.wallPost.Name = "wallPost";
+            this.wallPost.Size = new System.Drawing.Size(276, 192);
+            this.wallPost.TabIndex = 7;
+            // 
+            // getwallpost
+            // 
+            this.getwallpost.Location = new System.Drawing.Point(310, 275);
+            this.getwallpost.Name = "getwallpost";
+            this.getwallpost.Size = new System.Drawing.Size(164, 42);
+            this.getwallpost.TabIndex = 8;
+            this.getwallpost.Text = "Запись со стены пользователя";
+            this.getwallpost.UseVisualStyleBackColor = true;
+            this.getwallpost.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.getwallpost);
+            this.Controls.Add(this.wallPost);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -156,6 +176,8 @@ namespace VK_API_zadaniya
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox wallPost;
+        private System.Windows.Forms.Button getwallpost;
     }
 }
 
