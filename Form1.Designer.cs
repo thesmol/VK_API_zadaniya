0bb4f8636fd5376640c8616e612890480a8fd252
Ваня Медведев <ranzar2000@gmail.com>
2021-07-06 11:39:02 +0300

    Добавьте кнопки токена и текстбокс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,14 +31,15 @@ namespace VK_API_zadaniya
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 44);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(301, 35);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 54);
+            this.button1.Size = new System.Drawing.Size(164, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Показать список друзей";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,24 +48,43 @@ namespace VK_API_zadaniya
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(540, 44);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(480, 35);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(308, 404);
+            this.listBox1.Size = new System.Drawing.Size(276, 356);
             this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = " Токен пользователя";
+            this.button2.UseVisualStyleBackColor = true;
+
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Токен сообщества";
+            this.button3.UseVisualStyleBackColor = true;
+
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -73,6 +93,8 @@ namespace VK_API_zadaniya
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
